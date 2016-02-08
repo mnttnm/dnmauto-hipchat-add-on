456260ab84190d09255e67c19254cca2034cc537
Mohit <mohittater@bluejeansnet.com>
2016-02-08 23:08:49 +0530

    improvement in code for Run commands and code inclusion for Stop too

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var request = require('request');
 var cors = require('cors');
 var uuid = require('uuid');
 var fs = require('fs'); 
+var u_ = require('underscore');
+var http = require('http');
+var Promise = require("bluebird");
 
 // This is the heart of your HipChat Connect add-on. For more information,
 // take a look at https://developer.atlassian.com/hipchat/guide
@@ -56,15 +59,8 @@ module.exports = function (app, addon) {
       res.json({
         "label": {
           "type": "html",
-          "value": "Hello World!"
+          "value": "Denim Automator"
         },
-        "status": {
-          "type": "lozenge",
-          "value": {
-            "label": "Broken",
-            "type": "error"
-          }
-        }
       });
     }
   );
@@ -80,7 +76,7 @@ module.exports = function (app, addon) {
       res.json({
         "label": {
           "type": "html",
-          "value": "Hello World!"
+          "value": "Denim Automator"
         },
         "status": {
           "type": "lozenge",
@@ -142,17 +138,8 @@ module.exports = function (app, addon) {
 
 
   //Objects used in the code
-    var buildUrlObject = {};;
-      buildUrlObject.buildUrl = "";
-      buildUrlObject.message = "";
-
-    var commandInfo ={};
-      commandInfo.command = "";
-      commandInfo.operation = "";
-      commandInfo.environment = "";
-      commandInfo.project = "";
-
   var jobList = require("../jobList.json");
+    
 
   // This is an example route to handle an incoming webhook
   // https://developer.atlassian.com/hipchat/guide/webhooks
@@ -162,9 +149,9 @@ module.exports = function (app, addon) {
 
       var command = req.body.item.message.message;  //command will get the complete message passed from hipchat.
 
+      var buildUrlObject = generateBuildUrl(command);
 
-      buildUrlObject = generateBuildUrl(command);
-      if (buildUrlObject && buildUrlObject.buildUrl.length > 0 )  {
+      if (Object.keys(buildUrlObject).length !== 0)  {
         var options = {
           options : {
             color: "green"
@@ -172,7 +159,25 @@ module.exports = function (app, addon) {
         };
           hipchat.sendMessage(req.clientInfo, req.context.item.room.id,buildUrlObject.message,options);
 
-          request(
+          if (buildUrlObject.buildUrl.constructor === Array ) {
+            u_.each(buildUrlObject.buildUrl, function(buildUrl) {
+                request(
+                    {
+                      url:buildUrl,
+                      method: 'POST'
+                    }, 
+                    function (error, response, body) {
+                       if (!error && response.statusCode == 201) {
+                       }
+                       else {
+
+                       }
+                });
+            });
+          }
+
+          else {
+            request(
               {
                 url:buildUrlObject.buildUrl,
                 method: 'POST'
@@ -181,6 +186,7 @@ module.exports = function (app, addon) {
                  if (!error && response.statusCode == 201) {
                  }
             });
+          }
       }
 
       else {
@@ -190,36 +196,53 @@ module.exports = function (app, addon) {
             color: "red"
           }
         };
-        var msg = "Hang on, There is Some issue!";
-        if(buildUrlObject && buildUrlObject.message) {
-          msg = buildUrlObject.message;
-        }
+        var msg = "Hang on! Some issue, Try one more time with correct command";
         hipchat.sendMessage(req.clientInfo, req.context.item.room.id,msg,options);
-
       }
     });
 
+  function getAllBuilds() {
+    var buildArray = [];
+    for (var key in jobList) {
+      if (jobList.hasOwnProperty(key)) {
+        var job = jobList[key];
+        console.log(key + ": " + JSON.stringify(job));
+        if(job['includeInSanity']) {
+          buildArray.push(job);
+        }
+      }
+    }
+    console.log('Identified ' + buildArray.length + ' builds to include in denim');
+    return buildArray;
+  }
 
   function generateBuildUrl(command) {
+      var commandInfo = {};
       commandInfo = parseCommand(command);
+      var buildUrlObject = {};
       //We can have one more parameter which can have values like http reponsecode for indicating that this function was successfurl or not.
 
-      if( commandInfo && commandInfo.command === "") {
-        console.log("command info is empty");
-        buildUrlObject.message = "Invalid command, Missing some parameters!";
+      if(Object.keys(commandInfo).length == 0) {
+        console.log("command: " + command + " is invalid or Missing some parameters");
         return buildUrlObject;
       }
 
       else {
         //based on run/stop or status we will form our urls
         switch (commandInfo.command.toLowerCase())  {
-          case "run": 
-              if(commandInfo) {
+            case "run": 
                 switch (commandInfo.project){
                     case "denim":
                           buildUrlObject.message = "Denim automation will run " + commandInfo.operation + " on " + commandInfo.environment; 
                           buildUrlObject.buildUrl = []; //will return array of build urls for denim.  
+                          var builds = getAllBuilds();
+                          if (builds.constructor === Array ) {
+                                    u_.each(builds, function(build) {
+                                         buildUrlObject.buildUrl.push(getFinalBuildUrl(build,commandInfo))
+                                    });
+                          }
                           break;  
+
                     default:
                           console.log("generating build url for " + commandInfo.project);
                           buildUrlObject.message = "Denim automation will run " + commandInfo.operation + " on " + commandInfo.environment + " for " + commandInfo.project; 
@@ -227,69 +250,209 @@ module.exports = function (app, addon) {
                               var currentProject = jobList[commandInfo.project];
                               console.log("project choosen is: " + JSON.stringify(currentProject));
                           }
-                          if((JSON.stringify(currentProject.Env)).indexOf('same') > -1) {
-                            console.log("en is saaaamee")
-                             var Env = commandInfo.environment;
-                          }
-                          else{
-                            Env = currentProject.Env[commandInfo.environment];
-                          }
-                          // var Env = (JSON.stringify(currentProject.Env)) == "same" ? commandInfo.environment : currentProject.Env[commandInfo.environment];
-                          var Tag;
-                          if(commandInfo.operation.toLowerCase().indexOf('smoke') > -1) {
-                            Tag = currentProject.smoke;
-                          }
-                          else if(commandInfo.operation.toLowerCase().indexOf('sanity') > -1) {
-                            Tag = currentProject.sanity;
-                          }
                           else {
                             console.log('Operation mentioned by you can not performed!');
-                          }
-                          buildUrlObject.buildUrl = currentProject.job_url + '?' + currentProject.mapping.Env + "=" + Env + "&" + currentProject.mapping.Tag + "=" +Tag;                    
+                          } 
+                          buildUrlObject.buildUrl = getFinalBuildUrl(currentProject,commandInfo);   
+                          break;                 
                  }
                 console.log(buildUrlObject);
                 return buildUrlObject;
-              }
-          case "stop" : console.log('stop function not present');
-                        break;
+                break;
+            case "stop" : 
+                  switch (commandInfo.project){
+                    case "denim":
+                          buildUrlObject.message = "Stopping all the jobs of " + commandInfo.project;
+                          buildUrlObject.buildUrl = []; //will return array of build urls for denim.  
+                          var builds = getAllBuilds();
+                          if (builds.constructor === Array ) {
+                                    u_.each(builds, function(build) {
+                                         buildUrlObject.buildUrl.push(getFinalBuildUrl(build,commandInfo))
+                                    });
+                          }
+                          break;  
+
+                    default:
+                          buildUrlObject.message = "Stopping all the jobs of " + commandInfo.project;
+                          if(jobList.hasOwnProperty(commandInfo.project)) {
+                              var jobUrl  = jobList[commandInfo.project]['job_url'];
+                              console.log("job url of projec is: " + JSON.stringify(jobUrl));
+                          }
+                          else {
+                            console.log('Operation mentioned by you can not performed!');
+                          } 
+                          buildUrlObject = getBuildStopUrl(jobUrl);   
+                          break;                 
+                  }
+                return buildUrlObject;
+                break;
 
           case "status":console.log('status function not present');
+                        return buildUrlObject;
                         break;
           default: console.log("command you entered are not present please try again!");
+                   return buildUrlObject;
+                   break;
         }
       }
   }
 
+  function getBuildStopUrl(jobUrl) {
+    var jobStatusApi = jobUrl + 'api/json';
+    var buildStopObject = [];
+    function getBuildStopObjct(buildStop) {
+         if(buildStop.buildId !== ""){
+            if(buildStop.isQueue) {
+              buildStopObject = [buildStop.buildId, true];
+            }
+            else{
+              buildStopObject = [buildStop.buildId, false];
+            }
+         }
+         console.log("build stop object: " + buildStopObject);
+         return buildStopObject;
+    }
+    return getBuildId(jobStatusApi, getBuildStopObjct);
+  }
+
+
+  function getBuildId(jobUrl, callback) {
+       var lastBuild = "";
+       var lastCompletedBuild = "";
+       var isQueue = false;
+
+       var buildId = [];
+
+        console.log('calling job api with url: ' + jobUrl)
+        request({url:jobUrl,method:'GET'}, 
+            function (error, response, body) {
+                 if (!error && response.statusCode == 200) {
+                      // console.log('json reponse for build api: '+body);
+                      lastBuild = JSON.parse(body).lastBuild.number;
+                      lastCompletedBuild = JSON.parse(body).lastCompletedBuild.number;
+                      isQueue = JSON.parse(body).inQueue;
+                      console.log("last_Build: " + lastBuild);
+                      console.log("lastCompletedBuild: " + lastCompletedBuild);
+                      console.log("ifQueue: "+ isQueue);
+                 }
+                 else {
+                      console.error(error);
+                 }
+                 if(lastBuild != lastCompletedBuild) {
+                  if(!isQueue) {
+                   callback ({
+                    "buildId" : lastBuild,
+                    "message" : "Only job running is with build no " + lastBuild,
+                    "isQueue" : false
+                   })
+                  }
+                  else {
+                   callback({
+                    "buildId" : lastBuild,
+                    "message" : "multiple job scheduled",
+                    "isQueue" : true
+                   })
+                  }
+                 }
+                else {
+                 callback( {
+                  "buildId" : "",
+                  "message" : "Nothing to abort!",
+                  "isQueue" : false
+                 })
+               }
+        });
+  }
 
   function parseCommand(cmd) {
-
+    var commandInfo = {};
     console.log('command received is: ' + cmd);
 
     var fullCommand = cmd.substr(cmd.indexOf(" ") + 1, cmd.length - 1);
 
     var commandArray = fullCommand.split(" ");
 
-    if(commandArray.length < 4) {
-      return commandInfo;
-    }
-
-    else {
-      commandInfo.command = commandArray[0];
-      commandInfo.operation = commandArray [1];
-      commandInfo.environment = commandArray [3];
-      commandInfo.project = "";
-      if(commandArray.length === 4) {
-       commandInfo.project = "Denim"
+    if(commandArray.length == 2 || commandArray.length == 6) {
+      if(commandArray.length == 2) {
+            var projectName = checkIfProjectPresent(commandArray[1]);
+            if(commandArray[0].toLowerCase() == 'stop' &&  projectName != false){
+             commandInfo.command = commandArray[0];
+             commandInfo.project = projectName;
+             return commandInfo;
+            }
+            else return commandInfo;
       }
       else {
-       commandInfo.project = commandArray[5];
+            var projectName = checkIfProjectPresent(commandArray[5]);
+            if(projectName != false) {
+                commandInfo.command = commandArray[0];
+                commandInfo.operation = commandArray [1];
+                commandInfo.environment = commandArray [3];
+                commandInfo.project = projectName;
+                console.log(commandInfo);
+                return commandInfo;
+            }
+            else return commandInfo;
       }
-      
-      console.log(commandInfo);
-
-      return commandInfo;
+    }  
+    else {
+        consol.log('returning commandInfo as: ' + commandInfo);
+        return commandInfo;
     }
   }
+
+  function checkIfProjectPresent(project) {
+    if(project.toLowerCase() == "denim") return project;
+
+    var isProjectValid = u_.find(Object.keys(jobList), function(key) {
+        return key.toLowerCase() === project.toLowerCase();
+    });
+    if(typeof isProjectValid == 'undefined') {
+      console.log('!!!! ' + project +' !!! is not a correct project name');
+      console.log('Please correct the project name');
+      return false;
+    }
+    else {
+      return isProjectValid;
+    }
+  }
+
+  function getFinalBuildUrl(currentProject,commandInfo){
+      var Tag = "";
+      if(commandInfo.operation.toLowerCase().indexOf('smoke') > -1) {
+         if(currentProject.mapping.Tag instanceof Array) {
+            u_.each(currentProject.mapping.Tag, function(tag){
+              Tag = Tag + "&" + tag + '=' + currentProject.smoke[tag];
+            });
+         }
+
+         else {
+              Tag = Tag +'&' + currentProject.mapping.Tag + '=' + currentProject.smoke;
+         }
+      }
+      else if(commandInfo.operation.toLowerCase().indexOf('sanity') > -1) {
+         if(currentProject.mapping.Tag instanceof Array) {
+            u_.each(currentProject.mapping.Tag, function(tag){
+                Tag = Tag + "&" + tag + '=' + currentProject.sanity[tag];
+            });
+         } 
+         else {
+              Tag = Tag + '&' + currentProject.mapping.Tag + '=' + currentProject.sanity;
+         }     
+      }
+
+     var Env;
+     if((JSON.stringify(currentProject['Env'])).indexOf('same') > -1) {
+         Env = commandInfo.environment;
+     }
+     else{
+         Env = currentProject.Env[commandInfo.environment];
+     }
+
+     return currentProject.job_url+'buildWithParameters'+ '?' + currentProject.mapping.Env + "=" + Env  + Tag;                   
+  }
+
+
 
   addon.on('installed', function(clientKey, clientInfo, req){
     var options = {
